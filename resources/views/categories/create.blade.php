3c68aa82b4e835c371db0e143578a675feb65c7c
AlexanedrVileyka <Talex9@mail.ru>
2024-02-16 12:10:14 +0300

    Стилизовали формы

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,14 +1,74 @@
-<form action="{{route('categories.store')}}" method="post" enctype="multipart/form-data">
-    @csrf
-    <label>
-        Название
-        <input type="text" name="name" id="name" required>
-    </label>
-    <label>
-        Постер
-        <input type="file" name="poster" id="poster" required>
-    </label>
-    <button type="submit">
-        <span>Сохранить</span>
-    </button>
-</form>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
+<body>
+<div class="container">
+    <header>
+        <h1>
+            <a href="#">
+                <img src="https://tfgms.com/sandbox/dailyui/logo-1.png" alt="Authentic Collection">
+            </a>
+        </h1>
+    </header>
+    <h1 class="text-center">Register</h1>
+
+    <form class="registration-form" action="{{route('categories.store')}}" method="post" enctype="multipart/form-data">
+        @csrf
+        <label class="col-one-half">
+            <span class="label-text">Название</span>
+            <input type="text" name="name" id="name" required>
+        </label>
+        <label class="col-one-half">
+            <span class="label-text">Постер</span>
+            <input type="file" name="poster" id="poster" required>
+        </label>
+        <button class="submit" type="submit">
+            <span>Сохранить</span>
+        </button>
+    </form>
+
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="{{ asset("assets/js/create.js") }}"></script>
+</div>
+</body>
+</html>
+____________________________________________________
+
+
+
+{{--<form class="registration-form">--}}
+{{--    <label class="col-one-half">--}}
+{{--        <span class="label-text">First Name</span>--}}
+{{--        <input type="text" name="firstName">--}}
+{{--    </label>--}}
+{{--    <label class="col-one-half">--}}
+{{--        <span class="label-text">Last Name</span>--}}
+{{--        <input type="text" name="lastName">--}}
+{{--    </label>--}}
+{{--    <label>--}}
+{{--        <span class="label-text">Email</span>--}}
+{{--        <input type="text" name="email">--}}
+{{--    </label>--}}
+{{--    <label class="password">--}}
+{{--        <span class="label-text">Password</span>--}}
+{{--        <button class="toggle-visibility" title="toggle password visibility" tabindex="-1">--}}
+{{--            <span class="glyphicon glyphicon-eye-close"></span>--}}
+{{--        </button>--}}
+{{--        <input type="password" name="password">--}}
+{{--    </label>--}}
+{{--    <label class="checkbox">--}}
+{{--        <input type="checkbox" name="newsletter">--}}
+{{--        <span>Sign me up for the weekly newsletter.</span>--}}
+{{--    </label>--}}
+{{--    <div class="text-center">--}}
+{{--        <button class="submit" name="register">Sign Me Up</button>--}}
+{{--    </div>--}}
+{{--</form>--}}
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,36 +1,63 @@
-<form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
-@csrf
-    <label>
-        Название
-        <input type='text' name="name" placeholder="название поста" required>
-    </label>
-    <label>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
+<body>
+<div class="container">
+    <header>
+        <h1>
+            <a href="#">
+                <img src="https://tfgms.com/sandbox/dailyui/logo-1.png" alt="Authentic Collection">
+            </a>
+        </h1>
+    </header>
+    <h1 class="text-center">Категории постов</h1>
 
-       Описание
-        <input type='text' name="description" placeholder="название поста">
-    </label>
+    <form class="registration-form" action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <label class="col-one-half">
+            <span class="label-text">Название</span>
+            <input type='text' name="name" placeholder="Название поста" required>
+        </label>
 
-    <label>
-        Содержание
-       <textarea name="content" id="content" cols="30" rows="10" required></textarea>
-    </label>
+        <label class="col-one-half">
+            <span class="label-text">Описание</span>
+            <input type='text' name="description" placeholder="Описание поста">
+        </label>
 
-    <label>
-        Постер
-        <input type="file" name="poster" id="poster" required>
-    </label>
+        <label class="col-one-half">
+            <span class="label-text">Содержание</span>
+            <textarea name="content" id="content" cols="30" rows="10" required></textarea>
+        </label>
 
-<label>
-    Категория
-    <select name="category_ids[]" id="category_ids" multiple>
-        @foreach($categories as $category)
-            <option value="{{ $category->id }}"> {{ $category->name }}</option>
-        @endforeach
+        <label class="col-one-half">
+            <span class="label-text">Постер</span>
+            <input type="file" name="poster" id="poster" required>
+        </label>
 
-    </select>
-</label>
+        <label class="col-one-half">
+            <span class="label-text">Категория</span>
+            <select name="category_ids[]" id="category_ids" multiple>
+                @foreach($categories as $category)
+                    <option value="{{ $category->id }}"> {{ $category->name }}</option>
+                @endforeach
 
-    <button type="submit">
-        <span>Сохранить</span>
-    </button>
-</form>
+            </select>
+        </label>
+
+        <button class="submit" type="submit">
+            <span>Сохранить</span>
+        </button>
+    </form>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="{{ asset("assets/js/create.js") }}"></script>
+</div>
+</body>
+</html>
