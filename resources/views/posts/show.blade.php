699f42f0b3c881d3364e7f118414a23b1251cd04
AlexanedrVileyka <Talex9@mail.ru>
2024-03-04 21:51:56 +0300

    Сщздали вывод коментариев. Создали возможность удалить комментарий. Начали делать авторизацию пользователя.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -71,6 +71,14 @@
                                                 <span>{{$comment->created_at->translateFormat('j F Y') }}</span>
                                             @endif
                                             <p>{{$comment->content}}</p>
+                                            <form action="{{ route('comments.destroy', $comment->id) }}"
+                                                  method="post"
+                                                  onsubmit="return confirm('Вы уверены?');"
+                                                  >
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit"><i class="fa fa-trash"></i> Удалить</button>
+                                            </form>
                                         </div>
                                     </div>
                                 @endforeach
@@ -80,7 +88,9 @@
 
                     <div class="blog-comment-form">
                         <h3>Оставить комментарий</h3>
-                        <form action="#" method="post">
+                        <form action="{{route('comments.store') }}" method="post">
+                            @csrf
+                            <input type="hidden" name="post_id" value="{{$post->id}}">
                             <input type="text" class="form-control" placeholder="Name" name="name" required>
                             <input type="email" class="form-control" placeholder="Email" name="email" required>
                             <textarea name="message" rows="5" class="form-control" id="message" placeholder="Message"
--- vite.config.js
+++ vite.config.js
@@ -6,26 +6,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/css/bootstrap.min.css',
-                'resources/css/font-awesome.min.css',
-                'resources/css/magnific-popup.css',
-
                 'resources/js/app.js',
-                'resources/js/custom.js',
-                'resources/js/bootstrap.min.js',
-                'resources/js/particles.min.js',
-                'resources/js/smoothscroll.js',
-                'resources/js/jquery.js',
-                'resources/js/jquery.parallax.js',
-
-
-                // 'resources/fonts/font-awesome.min.css',
-                // 'resources/fonts/FontAwesome.otf',
-                // 'resources/fonts/fontawesome-webfont.ttf',
-                // 'resources/fonts/fontawesome-webfont.woff',
-                // 'resources/fonts/fontawesome-webfont.woff2',
-                // 'resources/fonts/font-awesome.min.css',
-             ],
+            ],
             refresh: true,
         }),
     ],
