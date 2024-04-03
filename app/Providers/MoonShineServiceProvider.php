7113fb0e7ff49a566a4d9f04c5d8fc9f86bd15ff
AlexanedrVileyka <Talex9@mail.ru>
2024-04-03 19:03:41 +0300

    Ресурсы и меню для админки

---
+++ app/Providers/MoonShineServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use App\Models\Post;
+use App\MoonShine\Resources\CategoryResource;
+use App\MoonShine\Resources\PostResource;
+use MoonShine\Providers\MoonShineApplicationServiceProvider;
+use MoonShine\MoonShine;
+use MoonShine\Menu\MenuGroup;
+use MoonShine\Menu\MenuItem;
+use MoonShine\Resources\MoonShineUserResource;
+use MoonShine\Resources\MoonShineUserRoleResource;
+
+class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
+{
+    protected function resources(): array
+    {
+        return [];
+    }
+
+    protected function pages(): array
+    {
+        return [];
+    }
+
+    protected function menu(): array
+    {
+        return [
+            MenuGroup::make(static fn() => __('moonshine::ui.resource.system'), [
+                MenuItem::make(
+                    static fn() => __('moonshine::ui.resource.admins_title'),
+                    new MoonShineUserResource()
+                ),
+                MenuItem::make(
+                    static fn() => __('moonshine::ui.resource.role_title'),
+                    new MoonShineUserRoleResource()
+                ),
+            ]),
+            MenuGroup::make('Статьи', [
+                MenuItem::make('Посты', new PostResource())
+                    ->badge(fn() => Post::count())
+                    ->icon('hiroicons.outline.academic-cap'),
+                MenuItem::make('Категории', new CategoryResource()),
+            ]),
+        ];
+    }
+
+    /**
+     * @return array{css: string, colors: array, darkColors: array}
+     */
+    protected function theme(): array
+    {
+        return [];
+    }
+}
